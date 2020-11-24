ad5dea71ec735773c5e41fec5119717588face3e
Jenath Srikanth <201908660@post.au.dk>
2020-11-24 09:22:13 +0100

    funktion beskrevet

--- MiniProjekt_3/FilterSelect.m
+++ MiniProjekt_3/FilterSelect.m
@@ -1,25 +1,20 @@
 function [Bout,Aout] = FilterSelect(Fs, gain,Fc, sel)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-%Fs  = 44100;    % Sampelfrekvens                            
-%Fc = Fc/(Fs/2);
 K = tan(pi*Fc/Fs);
 V0 = 10^(gain/20);
-
 
 denomBoost = (1+sqrt(2)*K+K^2);
 denomCutLF = (V0+sqrt(2*V0)*K+K^2);
 denomCutHF = (1+sqrt(2*V0)*K+V0*K^2);
 
-%boost
 if(sel == "bass")
+    %boost
     if(gain >= 0)
         b0=(1+sqrt(2*V0)*K+V0*K^2)/denomBoost;
         b1=(2*(V0*K^2-1))/denomBoost;
         b2=(1-sqrt(2*V0)*K+V0*K^2)/denomBoost;
         a1=(2*(K^2-1))/denomBoost;
         a2=((1-sqrt(2)*K+K^2))/denomBoost;
-        
+     %cut
     elseif (gain < 0)
         b0=(V0*(1+sqrt(2)*K+K^2))/denomCutLF;
         b1=(2*V0*(K^2-1))/denomCutLF;
@@ -28,13 +23,14 @@ if(sel == "bass")
         a2=(V0-sqrt(2*V0)*K+K^2)/denomCutLF;        
     end
 elseif(sel == "treble")
+    %boost
     if(gain >= 0)
         b0=(V0+sqrt(2*V0)*K+K^2)/denomBoost;
         b1=(2*(K^2-V0))/denomBoost;
         b2=(V0-sqrt(2*V0)*K+K^2)/denomBoost;
         a1=(2*(K^2-1))/denomBoost;
         a2=((1-sqrt(2)*K+K^2))/denomBoost;
-        
+    %cut    
     elseif (gain < 0)
         b0=(V0*(1+sqrt(2)*K+K^2))/denomCutHF;
         b1=(2*V0*(K^2-1))/denomCutHF;
@@ -47,13 +43,6 @@ end
 B = [b0, b1, b2];
 A = [1, a1, a2];
 
-
-
-
 Aout = A;
 Bout = B;
 end
-
-%func(basgain, diskgain, bascut, diskcut){
-%    switch()
-%}
